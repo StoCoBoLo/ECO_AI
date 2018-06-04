1a225bf8bc4220ce726acc7d572002f9bf18205e
David <davidsolus@gmail.com>
2018-06-04 12:19:29 -0400

    Update Classroom.java

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
 
 public class Classroom {
-sgfgs
+	
+	private String name;
+	private String department;
+	private ArrayList<Appliance>appliances;
+	public Classroom(String name, String department, ArrayList<Appliance> appliances) {
+		super();
+		this.name = name;
+		this.department = department;
+		this.appliances = appliances;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public ArrayList<Appliance> getAppliances() {
+		return appliances;
+	}
+	public void setAppliances(ArrayList<Appliance> appliances) {
+		this.appliances = appliances;
+	}
+	
+	public double calcClassConsumption() {
+		double sum=0;
+		for(int i=0;i<appliances.size();i++) {
+			sum+=appliances.get(i).calcDailyConsumption();
+		}
+		return sum;
+		
+	}
+	
+	public ApplianceType findMaxApplianceType() {
+		return null;
+	
+	}
+	
 }
